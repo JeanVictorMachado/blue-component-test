ecee7ec1af208e9ebc3f7f3d8d24e6c5d8394d8a
JeanVictorMachado <jeanvictor.arq@gmail.com>
2023-03-02 23:37:10 -0300

    feat: creating grid structure

--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -1,20 +1,80 @@
 import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid'
 
 const rows: GridRowsProp = [
-  { id: 1, col1: 'Hello', col2: 'World' },
-  { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
-  { id: 3, col1: 'MUI', col2: 'is Amazing' },
+  {
+    id: 1,
+    col1: 'Hello',
+    col2: 'World',
+    col3: 'World',
+    col4: 'World',
+    col5: 'World',
+  },
+  { id: 2, col1: 'MUI X', col2: 'is awesome' },
+  { id: 3, col1: 'Material UI', col2: 'is amazing' },
+  { id: 4, col1: 'MUI', col2: '' },
+  { id: 5, col1: 'Joy UI', col2: 'is awesome' },
+  { id: 6, col1: 'MUI Base', col2: 'is amazing' },
+  { id: 7, col1: 'MUI Base', col2: 'is amazing' },
 ]
 
 const columns: GridColDef[] = [
-  { field: 'col1', headerName: 'Column 1', width: 150 },
-  { field: 'col2', headerName: 'Column 2', width: 150 },
+  { field: 'id', hide: true },
+  { field: 'col1', headerName: 'Column 1', flex: 1 },
+  { field: 'col2', headerName: 'Column 2', flex: 1 },
+  { field: 'col3', headerName: 'Column 3', flex: 1 },
+  { field: 'col4', headerName: 'Column 4', flex: 1 },
+  { field: 'col5', headerName: 'Column 5', flex: 1 },
 ]
 
 export const Grid = () => {
+  const styles = {
+    border: 'none',
+    borderRadius: '0px',
+    '.MuiDataGrid-columnHeaders': {
+      background: '#E4E8EA',
+    },
+    '.MuiDataGrid-columnHeader': {
+      background: '#E4E8EA',
+      borderColor: 'white',
+    },
+    '.MuiDataGrid-columnHeaderTitle': {
+      fontSize: '14px',
+      fontWeight: 400,
+      lineHeight: 20,
+      letter: '0.25px',
+      paddingLeft: '6px',
+    },
+    '.MuiDataGrid-cell': {
+      borderBottom: 'none',
+    },
+    '.MuiDataGrid-cell--textLeft': {
+      fontSize: '14px',
+      fontWeight: 400,
+      lineHeight: 20,
+      letter: '0.25px',
+      paddingLeft: '16px',
+    },
+    '.MuiDataGrid-columnSeparator': {
+      display: 'none',
+    },
+  }
+
   return (
-    <>
-      <DataGrid rows={rows} columns={columns} />
-    </>
+    <DataGrid
+      rows={rows}
+      columns={columns}
+      rowHeight={47}
+      headerHeight={47}
+      sx={styles}
+      autoHeight
+      showCellRightBorder
+      showColumnRightBorder
+      localeText={{
+        MuiTablePagination: {
+          labelRowsPerPage: 'Linhas por página:',
+          labelDisplayedRows: (texts: any) => `${texts.from} - ${texts.to} de ${texts.count}`,
+        },
+      }}
+    />
   )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,8 @@
+import { Box } from '@mui/material'
 import { Grid } from '~/components/Grid'
 
-export const Home = () => <Grid />
+export const Home = () => (
+  <Box padding={4}>
+    <Grid />
+  </Box>
+)
