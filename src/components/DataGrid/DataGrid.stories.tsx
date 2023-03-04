8c48a19566b180440fe2197348e394711df0ebc2
JeanVictorMachado <jeanvictor.arq@gmail.com>
2023-03-04 18:40:53 -0300

    feat: create stories

---
+++ src/components/DataGrid/DataGrid.stories.tsx
@@ -0,0 +1,55 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined'
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline'
+import { data } from '../../pages/Home/mock'
+import { DataGrid } from '.'
+import { ButtonsTest } from '../ButtonsTest'
+
+export default {
+  title: 'Example/DataGrid',
+  component: DataGrid,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof DataGrid>
+
+const Template: ComponentStory<typeof DataGrid> = (args) => <DataGrid {...args} />
+
+export const Default = Template.bind({})
+Default.args = { data }
+
+export const WithIcons = Template.bind({})
+WithIcons.args = {
+  data,
+  actions: {
+    columnName: 'Options',
+    elements: [
+      {
+        id: 'edit-id',
+        element: <EditOutlinedIcon />,
+        onClick: (params) => console.log({ edit: params }),
+      },
+      {
+        id: 'delete-id',
+        element: <DeleteOutlineIcon />,
+        onClick: (params) => console.log({ edit: params }),
+      },
+    ],
+  },
+}
+
+export const WithButtons = Template.bind({})
+WithButtons.args = {
+  data,
+  actions: {
+    cellWidth: 244,
+    columnName: 'Options',
+    elements: [
+      {
+        id: 'buttons-id',
+        element: <ButtonsTest onButtonClick={(id, params) => console.log(id, params)} />,
+      },
+    ],
+  },
+}
